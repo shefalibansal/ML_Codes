e8ed50f5b473ffbf20a862f1d397d8c602c79980
shefalibansal <bansalshefali11@gmail.com>
2019-06-21 16:18:39 +0530

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -61,7 +61,7 @@ looker.plugins.visualizations.add({
 
     // Grab the first cell of the data
     var firstRow = data[0];
-    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+    var firstCell = firstRow[queryResponse.fields.dimensions[1].name];
 
     // Insert the data into the page
     this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
